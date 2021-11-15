954a64ed0090ef7ca9fe6266cb0dd1cbe8933a09
huyphan05112005 <94151680+huyphan05112005@users.noreply.github.com>
2021-11-15 08:44:10 +0700

    Update baitap1234.cpp

--- baitap1234.cpp
+++ baitap1234.cpp
@@ -10,20 +10,20 @@ int main()
 	cin>>a>>b>>c;
 	//bai 1
 	if(a>0)
-		cout<<"a la so duong"<<endl;
+		cout<<"a la so duong."<<endl;
 	else
-		cout<<"a khong la so duong"<<endl;
+		cout<<"a khong la so duong."<<endl;
 	//bai 2
 	if(b%2==0)
-		cout<<"b la so chan"<<endl;
+		cout<<"b la so chan."<<endl;
 	else 
-		cout<<"b la so le"<<endl;
+		cout<<"b la so le."<<endl;
 	//bai 3
 	if((b!=0)&&(a%b==0))
-		cout<<"a la boi cua b va b la uoc cua a";
+		cout<<"a la boi cua b va b la uoc cua a.";
 	//bai 4
 	if(c<48!=c>57)
-		cout<<"Du lieu ban vua nhap"<<c<<" khong la mot ky tu so."<<endl;
+		cout<<"Du lieu ban vua nhap "<<c<<" khong la mot ky tu so."<<endl;
 	else
 		cout<<"Du lieu ban vua nhap "<<c<<" la mot ky so.";
 	return 0;
